be771e05490ca98f742318515d70d8a25402b187
raavinduf <ravinduf@rocketmail.com>
2020-03-24 16:07:03 +0530

    added task model need to add a template to show the tasks

--- Todo/settings.py
+++ Todo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'tasks.apps.TasksConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Colombo'
 
 USE_I18N = True
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    created_date=models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.title
