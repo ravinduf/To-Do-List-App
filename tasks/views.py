025ac320b0faeb26fdb212a427ed12ec77329680
raavinduf <ravinduf@rocketmail.com>
2020-03-25 10:13:33 +0530

    updated the template list.html to add tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import TaskForm
 # Create your views here.
 
 def index(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks/list.html',{'tasks':tasks})
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    return render(request, 'tasks/list.html',{'tasks':tasks,'form':form})
